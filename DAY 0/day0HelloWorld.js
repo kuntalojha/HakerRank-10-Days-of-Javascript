8935d51e2543256cfbf43dd71e890698b91bb110
Kuntal Ojha <kuntalojha2021@gmail.com>
2024-03-20 23:48:47 +0530

    Day 0

--- DAY 0/day0HelloWorld.js
+++ DAY 0/day0HelloWorld.js
@@ -6,41 +6,42 @@ process.stdin.setEncoding('utf-8');
 let inputString = '';
 let currentLine = 0;
 
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
+process.stdin.on('data', (inputStdin) => {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
+process.stdin.on('end', (_) => {
+  inputString = inputString
+    .trim()
+    .split('\n')
+    .map((string) => {
+      return string.trim();
     });
-    
-    main();    
+
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 /**
-*   A line of code that prints "Hello, World!" on a new line is provided in the editor. 
-*   Write a second line of code that prints the contents of 'parameterVariable' on a new line.
-*
-*	Parameter:
-*   parameterVariable - A string of text.
-**/
+ *   A line of code that prints "Hello, World!" on a new line is provided in the editor.
+ *   Write a second line of code that prints the contents of 'parameterVariable' on a new line.
+ *
+ *	Parameter:
+ *   parameterVariable - A string of text.
+ **/
 function greeting(parameterVariable) {
-    // This line prints 'Hello, World!' to the console:
-    console.log('Hello, World!');
-    // This is the answer
-    console.log(parameterVariable);
-    // Write a line of code that prints parameterVariable to stdout using console.log:
-    
+  // This line prints 'Hello, World!' to the console:
+  console.log('Hello, World!');
+  // This is the answer
+  console.log(parameterVariable);
+  // Write a line of code that prints parameterVariable to stdout using console.log:
 }
 
-
 function main() {
-    const parameterVariable = readLine();
-    
-    greeting(parameterVariable);
+  const parameterVariable = readLine();
+
+  greeting(parameterVariable);
 }
